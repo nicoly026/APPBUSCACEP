07a1f14bff53c330f258a2eb660c842406ac7cdc
Usuario <Usuario@CPD03-M01>
2023-02-14 16:00:32 -0300

    aula

--- APPBUSCACEP/APPBUSCACEP/Model/Bairro.cs
+++ APPBUSCACEP/APPBUSCACEP/Model/Bairro.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace APPBUSCACEP.Model
 {
-    internal class Bairro
+    public class Bairro
     {
+        public string descricao_bairro { get; set; }
     }
 }
--- APPBUSCACEP/APPBUSCACEP/Model/Cidade.cs
+++ APPBUSCACEP/APPBUSCACEP/Model/Cidade.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace APPBUSCACEP.Model
 {
-    internal class Cidade
+    public class Cidade
     {
+        public int id_cidade { get; set; }
+
+        public string descricao { get; set; }
+
+        public string uf { get; set; }
+
+        public string codigo_ibge { get; set; }
+
+        public int ddd { get; set; }
     }
 }
